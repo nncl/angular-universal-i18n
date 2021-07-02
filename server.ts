a916c7d33e476456e2400b7125c71052d486080c
Caue <nncl@live.com>
2021-07-02 09:30:15 -0300

    chore: Building app for each language

--- server.ts
+++ server.ts
@@ -11,16 +11,22 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import { LOCALE_ID } from '@angular/core';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
+export function app(lang: string): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/angular-universal-i18n/browser');
+  const distFolder = join(process.cwd(), `dist/angular-universal-i18n/browser/${ lang }`);
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule,
+    providers: [
+      {
+        provide: LOCALE_ID, useValue: lang
+      }
+    ]
   }));
 
   server.set('view engine', 'html');
@@ -35,7 +41,7 @@ export function app(): express.Express {
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, { req, providers: [ { provide: APP_BASE_HREF, useValue: req.baseUrl } ] });
   });
 
   return server;
@@ -44,10 +50,19 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env.PORT || 4000;
 
+  const appFr = app('fr');
+  const appEn = app('en');
+  const appPt = app('pt');
+
   // Start up the Node server
-  const server = app();
+  const server = express();
+  server.use('/fr', appFr);
+  server.use('/en', appEn);
+  server.use('/pt', appPt);
+  server.use('/', appPt);
+
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express server listening on http://localhost:${ port }`);
   });
 }
 
